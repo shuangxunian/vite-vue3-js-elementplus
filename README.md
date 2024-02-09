caedfa3d0a1fc3b6cede31764c2159836d138a98
shuangxunian <shuangxunian@gmail.com>
2024-02-10 01:27:10 +0800

    feat: vue-router pinia

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # 快速启动模板
 
 ## 技术栈
-vue3 + js + elementPlus + axios + vite
+vue3 + js + elementPlus + axios + vite + pinia
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia'
+import './assets/main.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
+import router from './router'
 import axios from 'axios'
 
 axios.defaults.baseURL = 'http://localhost:3000/'
@@ -10,5 +12,8 @@ axios.defaults.baseURL = 'http://localhost:3000/'
 const app = createApp(App)
 
 app.use(ElementPlus)
+app.use(createPinia())
+app.use(router)
 app.config.globalProperties.$http= axios
+
 app.mount('#app')
